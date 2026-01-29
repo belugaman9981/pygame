1c26920a038cab4422807b0498f6f0a6899fd179
belugaman9981 <littledage@outlook.com>
2026-01-28 21:22:55 -0800

    potato

--- Game.py
+++ Game.py
@@ -4,6 +4,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 640))
 
+potato_image = pygame.image.load("potato.png")
+
 running = True
 while running:
     for event in pygame.event.get():
