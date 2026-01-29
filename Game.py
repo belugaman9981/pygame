54e4cf0e8fdf20f49b2c5de45ab9930101508ce1
belugaman9981 <littledage@outlook.com>
2026-01-28 21:23:58 -0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,10 +4,16 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 640))
 
-potato_image = pygame.image.load("potato.png")
+potato_image = pygame.image.load("potato.png").convert()
 
 running = True
+x = 30
 while running:
+    
+    screen.blit(potato_image, (x, 30))
+    
+    x+=1
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
