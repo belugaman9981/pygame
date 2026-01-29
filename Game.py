40609b8c86191e32b03e78412c9d963ba335464b
belugaman9981 <littledage@outlook.com>
2026-01-28 21:28:13 -0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((640, 640))
 
 potato_image = pygame.image.load("potato.png").convert()
+potato_image = ((potato_image.get_width // 2), (potato_image.get_height // 2))
 
 running = True
 x = 30
@@ -14,7 +15,7 @@ deltaT = .1
 
 while running:
     
-    screen.blit(potato_image, (x, 30))
+    screen.blit(potato_image, (x, 0))
     
     x += 50 * deltaT
     
