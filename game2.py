aa188a50cbcd4d7fc47a1a35a054876494b80d1d
belugaman9981 <littledage@outlook.com>
2026-01-29 21:16:08 -0800

    Update game2.py

--- game2.py
+++ game2.py
@@ -2,28 +2,9 @@ import pygame
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-pygame.init()
+app = Ursina()
 
-window = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("The Jumping Man")
-
-x = 50
-y = 50
-
-width = 40
-height = 60
-
-velocity = 5
-
-
-running = True
-while running:
-    pygame.time.delay(100)
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-    pygame.draw.
-            
+window.title = "The Jumping Man"
+window.borderless = False
+window.exit_button._visible = True
+window.fps_counter.enabled  = True
