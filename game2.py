b365157fab29f8327723652e8eb4124090eb5bd0
belugaman9981 <littledage@outlook.com>
2026-01-29 21:20:57 -0800

    Update game2.py

--- game2.py
+++ game2.py
@@ -8,3 +8,20 @@ window.title = "The Jumping Man"
 window.borderless = False
 window.exit_button._visible = True
 window.fps_counter.enabled  = True
+
+Sky(color= color.cyan)
+
+ground = Entity(
+    model='plane',
+    scale=500,
+    texture='white_cube',
+    texture_scale=(100, 100),
+    color=color.rgb(170, 80, 50),
+    collider='box'
+)
+
+player = FirstPersonController(
+    y=2,
+    speed=6,
+    jump_height=1.5
+)
