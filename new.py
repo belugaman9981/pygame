956518d9e54b115cbb581bffa303a9a47b267a18
belugaman9981 <littledage@outlook.com>
2026-01-29 20:53:02 -0800

    Update new.py

--- new.py
+++ new.py
@@ -6,6 +6,15 @@ window = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("The Jumping Man")
 
+x = 50
+y = 50
+
+width = 40
+height = 60
+
+velocity = 5
+
+
 running = True
 while running:
     for event in pygame.event.get():
